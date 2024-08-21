cc6cabb01f37844cc19f7b79fb9bebb7c377816f
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-08-21 18:28:26 +0300

    The concept of abstraction was illustrated using the 'Shape' application.

---
+++ Cpp/shape/main.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+class Shape{
+protected:
+    int x;
+    int y;
+
+public:
+    int getX(){
+        return x;
+    }
+    int getY(){
+        return y;
+    }
+    void setX(int value){
+        x = value;
+    }
+    void setY(int value){
+        y = value;
+    }
+    Shape(int x, int y){
+        this->x = x;
+        this->y = y;
+    }
+    virtual void draw() = 0;
+};
+
+class Triangle : public Shape{
+public:
+    Triangle(int x, int y) : Shape(x, y){
+        
+    }
+    void draw() override{
+        cout << "The triangle (" << this->x << "," << this->y  <<") has been drawn." << endl;
+    }
+};
+
+class Rectangle : public Shape{
+public:
+    Rectangle(int x, int y) : Shape(x, y){
+
+    }
+    void draw() override{
+        cout << "The rectangle (" << this->x << "," << this->y  <<") has been drawn." << endl;
+    }
+
+};
+
+class Square : public Rectangle{
+public:
+    Square(int x) : Rectangle(x, x){
+
+    }
+    void draw() override{
+        cout << "The square (" << this->x << "," << this->y  <<") has been drawn." << endl;
+    }
+};
+
+int main(){
+
+    Triangle item1 = Triangle(3,5);  
+    Rectangle item2 = Rectangle(6, 8);
+    Square item3 = Square(5);
+
+    item1.draw();
+    item2.draw();
+    item3.draw();
+
+    return 0;
+}

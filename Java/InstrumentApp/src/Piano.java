041b5cb9a90f4f7efb9f9469dfd505e5a8069144
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-09-03 12:47:28 +0300

    The 'Piano' class was extended to implement the KeyboardInstrument interface.

--- Java/InstrumentApp/src/Piano.java
+++ Java/InstrumentApp/src/Piano.java
@@ -1,8 +1,13 @@
-public class Piano implements Instrument {
+public class Piano implements Instrument, KeyboardInstrument {
 
     @Override
     public void play() {
         System.out.println("Playing the piano...");
+    }
+
+    @Override
+    public void press() {
+        System.out.println("The key has been pressed.");
     }
     
 }

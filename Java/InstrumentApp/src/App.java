ef2d2df094591eaf25789c17c87b91bb7ad053b9
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-09-03 13:02:13 +0300

    In the main block, the 'playAll()' method of the 'Orchestra' class was called, and the program's functionality was tested.

--- Java/InstrumentApp/src/App.java
+++ Java/InstrumentApp/src/App.java
@@ -1,12 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Guitar guitar = new Guitar();
-        Instrument piano = new Piano();
-
-        guitar.play();
-        guitar.setChord();
-        piano.play();
+        Orchestra orchestra = new Orchestra();
+        orchestra.playAll();
 
     }
 }

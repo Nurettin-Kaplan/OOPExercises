2a82e48ee97829cdc346416216e1cf21f9ff769d
Nurettin-Kaplan <nurettinkaplaan@gmail.com>
2024-09-04 11:33:09 +0300

    The 'MathHelper' class was coded and tested within the main block.

--- Java/mathHelper/src/App.java
+++ Java/mathHelper/src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int result = MathHelper.add(3, 5);
+        System.out.println(result);
+
+        result = MathHelper.substract(10, 7);
+        System.out.println(result);
+
+        result = MathHelper.multipy(2, 8);
+        System.out.println(result);
+
+        result = MathHelper.divide(10, 5);
+        System.out.println(result);
     }
 }
